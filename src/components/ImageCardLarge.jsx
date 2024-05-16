598261a928d56580cd765d0c7b9415471028b99a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-05-16 14:49:47 +0530

    feat(styles git push)

--- src/components/ImageCardLarge.jsx
+++ src/components/ImageCardLarge.jsx
@@ -1,23 +1,25 @@
 import { faDollarSign, faDotCircle, faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import style from '../styles/imageCardLarge.module.css'
+
 function ImageCardLarge() {
     return (
-        <div className='imageCardLargeContainer'>
+        <div className={style.imageCardLargeContainer}>
             <img src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?q=80&w=699&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <div className='headdingTopContainer'>
+            <div className={style.headdingTopContainer}>
                 <p>Biriyani</p>
-                <div className='iconBackground'>
+                <div className={style.iconBackground}>
                     <FontAwesomeIcon icon={faStar} color='#FFD600' />
                     <p>2.4</p>
                 </div>
             </div>
-            <div className='headdingBottomContainer'>
-                <div className='iconBackground'>
+            <div className={style.headdingBottomContainer}>
+                <div className={style.iconBackground}>
                     <FontAwesomeIcon icon={faDotCircle} size='sm' color='green' />
                     <p>Veg</p>
                 </div>
-                <div className="iconBackground">
+                <div className={style.iconBackground}>
                     <FontAwesomeIcon icon={faDollarSign} />
                     <p>124</p>
                 </div>
